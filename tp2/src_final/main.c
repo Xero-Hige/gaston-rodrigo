13e8e0d6a0f624d5e229d1319876742b5a2203c2
rodrigoburdet <rodrigoburdet@gmail.com>
2014-05-15 04:24:42 +0000

    el main funciona como esperado

--- tp2/src_final/main.c
+++ tp2/src_final/main.c
@@ -13,23 +13,18 @@ void usage();
 void wc(int fd, int* lines, int* words, int* bytes); void version(char* nombre);
 
 int main(int argc, char* argv[]){
-	if (argc < 2){
+	if (argc < 1){
 		fprintf(stderr,"No se especifico ningun comando. Ingrese -h para mostrar la ayuda\n");
 		return 1;
 	}
 
 	TParseArg* args;
-	char *input = NULL;
-	int* res = NULL;
 	int fd;
 	int i=0;
 	int lines=0;
 	int words=0;
 	int bytes=0;
 	int flagsPassed = 0;
-	int flagPrintLines=1;
-	int flagPrintWords=1;
-	int flagPrintBytes=1;
 
 	// Creo el parseador de argumentos
 	args = ParseArg_new(5);
@@ -42,11 +37,18 @@ int main(int argc, char* argv[]){
 	ParseArg_addArg(args, NULL, 'c', "bytes", NULL, 0);
 	ParseArg_parse(args, argc, argv);
 
-	if( !(ParseArg_getArg(args,'V')) || !(ParseArg_getArg(args,'h')) || !(ParseArg_getArg(args,'w')) || !(ParseArg_getArg(args,'l') || !(ParseArg_getArg(args,'c')))){
-		fprintf(stderr,"Parametro invalido. Ingrese -h para mostrar la ayuda\n");
-		return 1;
+
+	//Si no tengo ninguno de los argumentos y tengo un -, entonces parametro invalido
+	if( !(ParseArg_getArg(args,'V')) && !(ParseArg_getArg(args,'h')) && !(ParseArg_getArg(args,'w')) && !(ParseArg_getArg(args,'l') && !(ParseArg_getArg(args,'c')))){
+		for ( i = 0 ; i < argc ; i++ ){ if (argv[i][0]=='-'){
+				fprintf(stderr,"Parametro invalido. Ingrese -h para mostrar la ayuda\n");
+				return 1;
+			}
+		}
 	}
 
+
+	
 	if(ParseArg_getArg(args, 'h')){
 		flagsPassed++;
 		usage(argv[0]);
@@ -63,76 +65,64 @@ int main(int argc, char* argv[]){
 
 	if(ParseArg_getArg(args, 'l')){
 		flagsPassed++;
-		flagPrintLines=0;
 	}
+
 
 	if(ParseArg_getArg(args, 'w')){
 		flagsPassed++;
-		flagPrintWords=0;
 	}
+
 
 	if(ParseArg_getArg(args, 'c')){
 		flagsPassed++;
-		flagPrintBytes=0;
 	}
 
 	//Si la diferencia entre argumentos y flags es mayor a 2 entonces
 	//tengo mas de 1 archivo y los leo todos 
-	if ( 1 + argc - flagsPassed > 2 ){
-		int linesTotal = 0;
-		int wordsTotal = 0;
-		int bytesTotal = 0;
-		for ( i = flagsPassed+1 ; i < argc ; i++ ){
-			char* file;
-			file = argv[argc+flagsPassed-i];
-			fd = open(argv[argc+flagsPassed-i],O_RDONLY);
-			wc(fd, &lines, &words ,&bytes);
-			linesTotal = linesTotal + lines;
-			wordsTotal = wordsTotal + words;
-			bytesTotal = bytesTotal + bytes;
-			if (flagPrintLines) {
-				printf("\n %d ",lines);
-			}
-			if (flagPrintWords) {
-				printf(" %d ",words);
-			}
-			if (flagPrintBytes) {
-				printf(" %d ",bytes);
-			}
-			printf(" %s \n", file);
-		}
-		if (flagPrintLines) {
-			printf(" %d ",linesTotal);
-		}
-		if (flagPrintWords) {
-			printf(" %d ",wordsTotal);
-		}
-		if (flagPrintBytes) {
-			printf(" %d ",bytesTotal);
-		}
-		printf(" total \n");
-	}
+	//if ( 1 + argc - flagsPassed >= 2 ){
+	int linesTotal = 0;
+	int wordsTotal = 0;
+	int bytesTotal = 0;
 
-	//Si la diferencia es 2, puede ser un archivo o stdin
-	if ( 1 + argc - flagsPassed == 2 ){
+	// Supongo que los archivos siempre estan al final (dsp de los argumentos)
+	// Loopeo por todos los archivos 
+	for ( i = flagsPassed+1 ; i < argc ; i++ ){
 		char* file;
-		file = argv[1+argc-flagsPassed];
+		file = argv[argc+flagsPassed-i];
 		fd = open(argv[argc+flagsPassed-i],O_RDONLY);
 		wc(fd, &lines, &words ,&bytes);
-		if (flagPrintLines) {
-			printf("\n %d ",lines);
-		}
-		if (flagPrintWords) {
-			printf(" %d ",words);
-		}
-		if (flagPrintBytes) {
-			printf(" %d ",bytes);
-		}
-		printf(" %s \n", file);
-	}
 
-	free(res);
-	free(input);
+		if(ParseArg_getArg(args, 'l')){
+			printf("Lines: %d \t ",lines);
+		}
+
+		if(ParseArg_getArg(args, 'w')){
+			printf("words: %d \t ",words);
+		}
+
+		if(ParseArg_getArg(args, 'c')){
+			printf("bytes: %d \t ",words);
+		}
+
+		//Por defecto ejecuta esta 
+		if (flagsPassed==0){
+			printf("Lines: %d \t words: %d \t bytes: %d \t",lines,words,bytes);
+		}
+
+		//Stdin:
+		if (fd==-1){
+			file = "";
+		}
+
+		printf("%s \n",file);
+
+		linesTotal = linesTotal + lines;
+		wordsTotal = wordsTotal + words;
+		bytesTotal = bytesTotal + bytes;
+
+	}
+	printf("Lines: %d \t words: %d \t bytes: %d \ttotal\n",linesTotal,wordsTotal,bytesTotal);
+
 	ParseArg_delete(args);
 
 	return 0;
